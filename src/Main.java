d2b7d91cf4645a7d8f69faf9b2ee1829a948c1b7
Kirill Kozhevnikov <kkojevnikov1993@gmail.com>
2022-11-26 18:13:47 +0700

    change

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ваше количество миль");
+
+        int t = 9000;
+        int m = 20;
+        int c = t / m;
+
+        System.out.println(c);
+    }
+}
